b81b99398a189b4bba5e663e93942999c1ee6b1c
mario55c6 <mario1999sanchez@gmail.com>
2023-07-12 17:14:24 +0200

    Extension de music en el módulo herenciav1

--- addons/herenciav1/models/models.py
+++ addons/herenciav1/models/models.py
@@ -2,8 +2,8 @@
 
 from odoo import models, fields
 
-class Music(models.Model):
+class Songs(models.Model):
     _inherit = "music.songs"
-    _name = "herenciav1.canciones"
     
     duration = fields.Char(string="Duración")
+    

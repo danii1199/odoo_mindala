9521f445f09fa05953656cc992fa100bc3af3894
mario55c6 <mario1999sanchez@gmail.com>
2023-07-20 13:02:59 +0200

    Quitar prueba

--- addons/mindala/static/src/js/systray_items/new_content.js
+++ addons/mindala/static/src/js/systray_items/new_content.js
@@ -1,15 +1,21 @@
 /** @odoo-module **/
 
-import { NewContentModal, MODULE_STATUS } from '@website/systray_items/new_content';
+import {
+  NewContentModal,
+  MODULE_STATUS,
+} from '@website/systray_items/new_content';
 import { patch } from 'web.utils';
 
 patch(NewContentModal.prototype, 'mindala_m4pnews_new_content', {
-    setup() {
-        this._super();
+  setup() {
+    this._super();
 
-        const newBlogElement = this.state.newContentElements.find(element => element.moduleXmlId === 'base.module_website_blog_news_prueba');
-        newBlogElement.createNewContent = () => this.onAddContent('mindala.blog_m4pnews_action_add', true);
-        newBlogElement.status = MODULE_STATUS.INSTALLED;
-        newBlogElement.model = 'blog.m4pnews';
-    },
+    const newBlogElement = this.state.newContentElements.find(
+      (element) => element.moduleXmlId === 'base.module_website_blog_news'
+    );
+    newBlogElement.createNewContent = () =>
+      this.onAddContent('mindala.blog_m4pnews_action_add', true);
+    newBlogElement.status = MODULE_STATUS.INSTALLED;
+    newBlogElement.model = 'blog.m4pnews';
+  },
 });

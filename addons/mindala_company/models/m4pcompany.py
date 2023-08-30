d37d4ebd4985856eccf9e3fb794d1ed918d29e32
danii1199 <daniimanzano1199@gmail.com>
2023-08-30 16:55:01 +0200

    Comento relations para mas adelante

--- addons/mindala_company/models/m4pcompany.py
+++ addons/mindala_company/models/m4pcompany.py
@@ -1,10 +1,16 @@
 from odoo import api, models, fields, _
 from odoo.addons.http_routing.models.ir_http import slug, unslug
+import json
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class MindalaCompany(models.Model):
     _name = "mindala.m4pcompany"
     _description = "Mindala empresas"
     _inherit = "mindala.base"
+
+    
 
     def _compute_website_url(self):
         super(MindalaCompany, self)._compute_website_url()
